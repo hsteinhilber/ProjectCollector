03d8a50778da657b9199543f238965cb2c61e260
Harry Steinhilber <harry.steinhilber@live.com>
2018-05-25 12:03:48 -0400

    Add project files.

---
+++ ProjectCollector/ProjectCollector/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace ProjectCollector.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
